f0b831f5b34444ef54c7da0789c749e991da6b06
Adrians Uvis Valdmanis <adrians.uvis@gmail.com>
2025-01-13 11:46:51 +0200

    Add matrix multiplication for python

---
+++ python/matricu_reizinasana.py
@@ -0,0 +1,23 @@
+import time
+
+def matrix_multiply(a, b):
+    rows = len(a)
+    cols = len(b[0])
+    result = [[0 for _ in range(cols)] for _ in range(rows)]
+
+    for i in range(rows):
+        for j in range(cols):
+            for k in range(len(a[0])):
+                result[i][j] += a[i][k] * b[k][j]
+    return result
+
+def main():
+    size = 100
+
+    a = [[1 for _ in range(size)] for _ in range(size)]
+    b = [[2 for _ in range(size)] for _ in range(size)]
+
+    _ = matrix_multiply(a, b)
+
+if __name__ == "__main__":
+    main()
